d39d776264519a59143a7e6efba382a981b78a45
gulumit <94010321+gulumit@users.noreply.github.com>
2021-12-11 18:59:27 +0300

    Update session2.sql

--- session2.sql
+++ session2.sql
@@ -106,13 +106,19 @@
 	/* customers tablosunda, isimleri (FirstName) üç harfli olan müşterilerin FirstName, 
 	LastName ve City bilgilerini	listeleyen sorguyu yazınız*/
 	
+	SELECT FirstName, LastName, City
+	FROM customers
+	WHERE FirstName LIKE '___';	
 	
 	/* customers tablosunda, soyisimleri Sch veya Go ile başlayan müşterilerin FirstName, 
 	LastName ve City bilgilerini listeleyen sorguyu yazınız*/
 	
+	SELECT FirstName, LastName, City
+	FROM customers
+	WHERE LastName LIKE 'Sch%' 
+	OR LastName LIKE 'Go%';
 	
-	
-	
+		
 	
 	
 	
