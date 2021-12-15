bcd0c87573611c315ef1ee70314ac53df20043f6
profnoah <noah@clarusway.com>
2021-12-16 00:27:23 +0300

    session3 added

--- Session3.sql
+++ Session3.sql
@@ -61,7 +61,7 @@
 	SELECT Name, Milliseconds
 	FROM tracks
 	WHERE Milliseconds > ( 	SELECT AVG(Milliseconds)
-										FROM tracks);
+																								FROM tracks);
 					
 /*===================================================
  GROUP BY
@@ -96,7 +96,7 @@
 	GROUP BY BillingCountry;
 		
 /*===================================================
-        JOINS
+ JOINS
 ====================================================*/
     
 --     Join islemleri farkli tablolardan secilen sutunlar ile yeni bir tablo 
@@ -129,43 +129,3 @@
 	 ON markalar.marka_id = siparisler.marka_id;
  
  
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-/*===================================================
-        JOINS
-====================================================*/
-    
---     Join islemleri farkli tablolardan secilen sutunlar ile yeni bir tablo 
---     olusturmak icin kullanilabilir.
---     
---     JOIN islemleri Iliskisel Veritabanlari icin cok onemli bir ozelliktir. Çunku
---    	Foreign Key'ler ile iliskili olan tablolardan istenilen sutunlari cekmek 
---     icin JOIN islemleri kullanilabilir.
-
---     Standart SQL'de en çok kullanılan Join islemleri:
---   	1) FULL JOIN:  Tablodaki tum sonuclari gosterir
---     2) INNER JOIN:  Tablolardaki ortak olan sonuc kumesini gosterir
---     3) LEFT JOIN:  Ilk tabloda (Sol) olup digerinde olmayan sonuclari gosterir
---     4) RIGHT JOIN: Sadece Ikinci tabloda olan tum sonuclari gosterir.
-
---		NOT: SQLite Sadece INNER, LEFT VE CROSS JOIN İşlemlerini desteklemektedir.
- 
- /*===================================================*/   
-
-  /* araclar.db adındaki veritabanını kullanarak Markalar ve Siparisler tablolarındaki 
- marka_id’si ayni olan kayıtların marka_id, marka_adi, siparis_adedi ve siparis_tarihi   
- bilgilerini  listeleyen bir sorgu yaziniz.*/
-	
-	
