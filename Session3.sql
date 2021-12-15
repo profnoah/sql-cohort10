c5497ea59bfa1bb9e86dee5086446b37f189f9ac
profnoah <noah@clarusway.com>
2021-12-16 00:30:49 +0300

    session3 added

--- Session3.sql
+++ Session3.sql
@@ -120,12 +120,13 @@
  marka_id’si ayni olan kayıtların marka_id, marka_adi, siparis_adedi ve siparis_tarihi   
  bilgilerini  listeleyen bir sorgu yaziniz.*/
  
- select * from markalar;
- 
-	 SELECT markalar.marka_id,markalar.marka_adi,
-				 siparisler.siparis_adedi, siparisler.siparis_tarihi
-	 FROM markalar
-	 LEFT JOIN siparisler 
-	 ON markalar.marka_id = siparisler.marka_id;
+ SELECT * FROM markalar;
+ SELECT * FROM siparisler;
+	
+	SELECT markalar.marka_id,markalar.marka_adi,
+				    siparisler.siparis_adedi, siparisler.siparis_tarihi
+	FROM markalar
+	INNER JOIN siparisler 
+	ON markalar.marka_id = siparisler.marka_id;
  
  
