c66021b853842966b055fe26ca9abf9337806515
profnoah <noah@clarusway.com>
2021-12-16 00:33:55 +0300

    session3 added

--- session3.sql
+++ session3.sql
@@ -122,11 +122,12 @@
  
  SELECT * FROM markalar;
  SELECT * FROM siparisler;
-	
-	SELECT markalar.marka_id,markalar.marka_adi,
-				    siparisler.siparis_adedi, siparisler.siparis_tarihi
-	FROM markalar
-	INNER JOIN siparisler 
-	ON markalar.marka_id = siparisler.marka_id;
+ 
+ SELECT markalar.marka_id,markalar.marka_adi,
+ siparisler.siparis_adedi, siparisler.siparis_tarihi
+ 
+ FROM markalar
+ INNER JOIN siparisler 
+ ON markalar.marka_id = siparisler.marka_id;
  
  
