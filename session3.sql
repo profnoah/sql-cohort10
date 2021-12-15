95a1197dedee3b04ab8b710cf0726b54d0d29d3f
profnoah <noah@clarusway.com>
2021-12-16 00:49:19 +0300

    join ödevleri eklendi

--- session3.sql
+++ session3.sql
@@ -129,3 +129,17 @@
  ON markalar.marka_id = siparisler.marka_id;
  
  
+
+/* ÖDEV: Chinook veritabanındaki tracks tablosunda bulunan her bir şarkının türü (genre)
+listeleyiniz.*/
+
+	 
+ /* ÖDEV: invoice tablosundaki faturaların her birinin müşteri adını (FirstName),
+ soyadını (lastName), fatura tarihi (InvoiceDate) ve fatura meblağını (total) 
+ listeleyen sorguyu yazınız */
+ 
+	
+	/* ÖDEV: artists tablosunda bulunan her bir kişinin albums tablosunda 
+bulunan tüm albümlerinin listeleyen sorguyu yazınız. 
+Sorguda ArtistId, Name, Title ve AlbumId olmalıdır */
+ 
